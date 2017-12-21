903146bd4ae660998310fbf1fae6491f4524f7d3
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-12-21 13:13:31 +0100

    Added more checks before picking up snowballs!
    You can now no longer pickup snowballs when the player is:
    1. inside an interrior
    2. inside a tunnel or underground
    3. shooting & walking at the same time without aiming
    4. taking cover
    5. in a scenario (No more coffee and doughnuts going to waste because you pickup a snowball by accident)

--- snowballs/client.lua
+++ snowballs/client.lua
@@ -17,7 +17,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(0) -- prevent crashing
         if IsNextWeatherType('XMAS') then -- check for xmas weather type
             RequestAnimDict('anim@mp_snowball') -- pre-load the animation
-            if IsControlJustReleased(0, 119) and not IsPedInAnyVehicle(GetPlayerPed(-1), true) and not IsPlayerFreeAiming(PlayerId()) and not IsPedSwimming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) then -- check if the snowball should be picked up
+            if IsControlJustReleased(0, 119) and not IsPedInAnyVehicle(GetPlayerPed(-1), true) and not IsPlayerFreeAiming(PlayerId()) and not IsPedSwimming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) and GetInteriorFromEntity(PlayerPedId()) == 0 and not IsPedShooting(PlayerPedId()) and not IsPedUsingAnyScenario(PlayerPedId()) and not IsPedInCover(PlayerPedId(), 0) then -- check if the snowball should be picked up
                 TaskPlayAnim(PlayerPedId(), 'anim@mp_snowball', 'pickup_snowball', 8.0, -1, -1, 0, 1, 0, 0, 0) -- pickup the snowball
                 Citizen.Wait(1950) -- wait 1.95 seconds to prevent spam clicking and getting a lot of snowballs without waiting for animatin to finish.
                 GiveWeaponToPed(GetPlayerPed(-1), GetHashKey('WEAPON_SNOWBALL'), 2, false, true) -- get 2 snowballs each time.
