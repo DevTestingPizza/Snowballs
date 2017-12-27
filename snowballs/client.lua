8971ed0fce5aaa430058189cee8a85f455bacb14
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-12-27 20:56:30 +0100

    Added snow particles and sound!
    Added snow particles and snow/ice sound effects for footsteps, vehicles and the snowball pickup animation! The snow particles caused by your vehicle tires now also add snow decals to the side of your vehicle.
    Preview
    ![](https://vespura.com/hi/i/7f45288c7e4.gif)

--- snowballs/client.lua
+++ snowballs/client.lua
@@ -7,15 +7,30 @@ local enableWeatherControl = false
 
 -- No need to touch anything below.
 Citizen.CreateThread(function()
-    showHelp = true
+    
+    local showHelp = true
+    local loaded = false
+    
     while true do
         if enableWeatherControl then
-            SetForceVehicleTrails(true)
-            SetForcePedFootstepsTracks(true)
             SetWeatherTypeNowPersist('XMAS')
         end
         Citizen.Wait(0) -- prevent crashing
         if IsNextWeatherType('XMAS') then -- check for xmas weather type
+            
+            SetForceVehicleTrails(true)
+            SetForcePedFootstepsTracks(true)
+            
+            if not loaded then
+                RequestScriptAudioBank("ICE_FOOTSTEPS", false)
+                RequestScriptAudioBank("SNOW_FOOTSTEPS", false)
+                RequestNamedPtfxAsset("core_snow")
+                while not HasNamedPtfxAssetLoaded("core_snow") do
+                    Citizen.Wait(0)
+                end
+                UseParticleFxAssetNextCall("core_snow")
+                loaded = true
+            end
             RequestAnimDict('anim@mp_snowball') -- pre-load the animation
             if IsControlJustReleased(0, 119) and not IsPedInAnyVehicle(GetPlayerPed(-1), true) and not IsPlayerFreeAiming(PlayerId()) and not IsPedSwimming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) and GetInteriorFromEntity(PlayerPedId()) == 0 and not IsPedShooting(PlayerPedId()) and not IsPedUsingAnyScenario(PlayerPedId()) and not IsPedInCover(PlayerPedId(), 0) then -- check if the snowball should be picked up
                 TaskPlayAnim(PlayerPedId(), 'anim@mp_snowball', 'pickup_snowball', 8.0, -1, -1, 0, 1, 0, 0, 0) -- pickup the snowball
@@ -30,14 +45,16 @@ Citizen.CreateThread(function()
             else
                 showHelp = true
             end
+        else
+            loaded = false
+            RemoveNamedPtfxAsset("core_snow")
+            ReleaseNamedScriptAudioBank("ICE_FOOTSTEPS")
+            ReleaseNamedScriptAudioBank("SNOW_FOOTSTEPS")
+            SetForceVehicleTrails(false)
+            SetForcePedFootstepsTracks(false)
         end
         if GetSelectedPedWeapon(PlayerPedId()) == GetHashKey('WEAPON_SNOWBALL') then
-            -- SetCanAttackFriendly(PlayerPedId(), false, false)
             SetPlayerWeaponDamageModifier(PlayerId(), 0.0)
-            SetPedSuffersCriticalHits(PlayerPedId(), false)
-        else
-            -- SetCanAttackFriendly(PlayerPedId(), true, false)
-            SetPedSuffersCriticalHits(PlayerPedId(), true)
         end
     end
 end)
